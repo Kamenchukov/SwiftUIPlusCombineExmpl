67430f808a3001ac43fd6ec86cf35fb0c91be129
Constantin <k.kamenchukov@gmail.com>
2022-07-06 11:24:08 +0300

    HW_7 SwiftUI + Combine Example

---
+++ SwiftUIPlusCombineExmpl/SwiftUIPlusCombineExmpl/SwiftUIPlusCombineExmplApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPlusCombineExmplApp.swift
+//  SwiftUIPlusCombineExmpl
+//
+//  Created by Константин Каменчуков on 06.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPlusCombineExmplApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
